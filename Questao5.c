0e0c26b4702026fdc2971c91e94cf4cad510cbbc
GRocha1 <30627476+GRocha1@users.noreply.github.com>
2017-08-17 14:42:33 -0300

    Add files via upload

--- Questao5.c
+++ Questao5.c
@@ -9,7 +9,7 @@ typedef struct {
     int Ranking;
 }ficha_banda;
 
-ficha_banda bandas[5];
+static ficha_banda bandas[5];
 
 void preenche_bandas(ficha_banda bandas[]){
     int i;
@@ -27,7 +27,7 @@ void preenche_bandas(ficha_banda bandas[]){
         printf("Nome da banda: %s\n", bandas[i].Nome_Banda);
         printf("Estilo musical: %s\n", bandas[i].Tipo_Musica);
         printf("Numero de integrantes: %d\n", bandas[i].N_integrantes);
-        printf("Ranking: %d", bandas[i].Ranking);
+        printf("Ranking: %d\n", bandas[i].Ranking);
     }
 }
 
@@ -39,17 +39,55 @@ void BuscarRanking(int j){
         printf("Nome da banda: %s\n", bandas[i].Nome_Banda);
         printf("Estilo musical: %s\n", bandas[i].Tipo_Musica);
         printf("Numero de integrantes: %d\n", bandas[i].N_integrantes);
-        printf("Ranking: %d", bandas[i].Ranking);
+        printf("Ranking: %d\n", bandas[i].Ranking);
         }
+    }
+}
+
+void BuscarEstilo(char *estilo){
+    int i, j = 0;
+    for(i = 0; i < 5; i++){
+        if(!(strcmp(bandas[i].Tipo_Musica, estilo))){
+            printf("\n%s\n", bandas[i].Nome_Banda);
+        }else{
+           j++;
+        }
+    }
+    if(j == 5){
+        puts("Nao foram encontradas bandas com esse estilo musical.");
+    }
+}
+
+void BuscarBandas(char *banda){
+    int i, j = 0;
+    for(i = 0; i < 5; i++){
+        if(!(strcmp(bandas[i].Nome_Banda, banda))){
+            printf("%s\n", bandas[i].Nome_Banda);
+        }else{
+           j++;
+        }
+    }
+    if(j == 5){
+        printf("A %s banda nao foi encontrada nos favoritos.\n", banda);
     }
 }
 
 int main(void){
     int j;
+    char estilo[51], banda1[51];
 
     preenche_bandas(bandas);
     printf("Digite um numero de 1 a 5: ");
-    scanf("%d", &j);
+    scanf("%d%*c", &j);
     BuscarRanking(j);
+    printf("Insira o estilo musical para encontrar bandas: ");
+    fgets(estilo, 51, stdin);
+    printf("Bandas do estilo musical %s encontradas: \n", estilo);
+    BuscarEstilo(estilo);
+    printf("Insira o nome da banda para procura-la nos favoritos: ");
+    fgets(banda1, 51, stdin);
+    printf("Resultado da busca: \n");
+    BuscarBandas(banda1);
+
     return 0;
 }
